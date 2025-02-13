c7f25fac0db7ddc4f545e4ad84a5ce1dad52a43b
Vaibhav Dharmik <vaibhavd2017@gmail.com>
2025-02-13 13:09:04 +0530

    Starter package

---
+++ src/main/java/com/practise/spring_boot_practise/SpringBootPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.practise.spring_boot_practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPractiseApplication.class, args);
+	}
+
+}
