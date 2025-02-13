b00bd0b3f63885c8c1058a0f8f33157a904f11fe
Vaibhav Dharmik <vaibhavd2017@gmail.com>
2025-02-13 13:56:46 +0530

    REST API returns List (JSON format)

--- src/main/java/com/practise/spring_boot_practise/controller/StudentController.java
+++ src/main/java/com/practise/spring_boot_practise/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.practise.spring_boot_practise.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -16,5 +19,16 @@ public class StudentController {
                 "Dharmik"
         );
     return student;
+    }
+
+    //    http://localhost:8080/students
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1,"Vaibhav","Dharmik"));
+        students.add(new Student(2,"Umesh","temp"));
+        students.add(new Student(3,"Ram","temp2"));
+        students.add(new Student(4,"Sanjay","temp3"));
+        return students;
     }
 }
